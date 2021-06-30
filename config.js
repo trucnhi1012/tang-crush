9f980b2f7675084042ab7e58401785183cf8ee08
trucnhi1012 <86711604+trucnhi1012@users.noreply.github.com>
2021-06-30 15:16:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim tôi lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích tn <33',
+    btnNo: 'Không đâu tn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu tn <3',
+    btnReply: 'Gửi cho tn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'tn biết mà 🥰. Yêu vợ nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 10h mình gặp nhau trên dis nhá công chúa, nhớ em lắm rồi đó.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
